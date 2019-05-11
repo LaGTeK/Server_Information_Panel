85f2f70d7582fdd3c998f19cd6a5ba6ac502dbba
LaGTeK <lagtek13012@gmail.com>
2019-05-11 20:07:22 +0200

    file for constants

---
+++ ServerPanel/scripts/5_Mission/Constants/Contants.c
@@ -0,0 +1,14 @@
+	static const string SP_CONFIGKEY_PATH 			= "$saves:ServerPanelKey.json";
+	static const string SP_CONFIG_PATH				= "$profile:\\ServerPanel\\ServerPanelConfig.json";
+	static const string TAB0_PATH 					= "$profile:\\ServerPanel\\ServerTab0.txt";
+	static const string TAB1_PATH 					= "$profile:\\ServerPanel\\ServerTab1.txt";
+	static const string TAB2_PATH 					= "$profile:\\ServerPanel\\ServerTab2.txt";
+	static const string TAB3_PATH 					= "$profile:\\ServerPanel\\ServerTab3.txt";
+	static const string PROFILE_PATH 				= "$profile:\\ServerPanel\\";
+
+	//OLD PATH
+	static const string SP_OLD_CONFIG_PATH			= "$profile:\\ServerPanel\\ServerPanel.json";
+	static const string SP_OLD_DESCRIPTION_PATH		= "$profile:\\ServerPanel\\ServerDescription.txt";
+	static const string SP_OLD_RULES_PATH			= "$profile:\\ServerPanel\\ServerRules.txt";
+
+	const int SERVER_PANEL							=	26169;
