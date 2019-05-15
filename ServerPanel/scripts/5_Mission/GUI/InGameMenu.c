e4a78e0d6a9f20681b96305d5ae84bae835ebf6a
LaGTeK <lagtek13012@gmail.com>
2019-05-15 20:28:04 +0200

    Lil fix

--- ServerPanel/scripts/5_Mission/GUI/InGameMenu.c
+++ ServerPanel/scripts/5_Mission/GUI/InGameMenu.c
@@ -1,22 +1,8 @@
 modded class InGameMenu {
 	override void RegisterButtons() {
+		//RegisterButton( "CREDITS", "OnClick_Credits" );
 
-		RegisterButton( "#main_menu_continue", "ContinueMission" );
-		RegisterButton( "#main_menu_exit", "QuitMission" );
-
-        if ( GetGame().IsMultiplayer() )
-        {
-		    RegisterButton( "#main_menu_respawn", "RespawnMission", 2 );
-        } else
-        {
-		    RegisterButton( "#main_menu_restart", "RestartMission" );
-        }
-
-		RegisterButton( "#layout_xbox_ingame_menu_options", "EnterOptionsMenu" );
-
-		RegisterButton( "CREDITS", "EnterCreditsMenu" );
-
-		RegisterButton( "SERVER'S INFORMATIONS", "EnterServerPanelMenu" );
+		RegisterButton( "SERVER INFORMATIONS", "EnterServerPanelMenu" );
     }
 
 	void EnterServerPanelMenu() {
